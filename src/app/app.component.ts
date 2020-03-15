f561ddfe308e9f1577255e9fc467f3db9cd36f85
AlirezaEbrahimkhani <alirezaebrahimkhani1973@gmail.com>
2020-03-15 14:01:33 +0330

    getch data from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Item, ApiService } from "./api.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'pwa-angular-demo';
+  title = "pwa-angular-demo";
+  items: Array<Item>;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.fetchData()
+  }
+
+  fetchData() {
+    this.apiService.fetch().subscribe(
+      (data: Array<Item>) => {
+        console.log(data);
+        this.items = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
